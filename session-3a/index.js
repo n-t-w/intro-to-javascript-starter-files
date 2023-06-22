7b877010464bcabadee4b029a15011016f41ab5a
Tabitha Ncooro <ncoorotw@yahoo.com>
2023-06-22 19:18:36 +0100

    Completed Task 3

--- session-3a/index.js
+++ session-3a/index.js
@@ -1,9 +1,9 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
 
-var a = 5;
-var b = 10;
-var c = a + b;
+let a = 5;
+const b = 10;
+let c = a + b;
 
 // Expected output 15
 console.log("Original output: " + c);
@@ -39,3 +39,6 @@ console.log("After c has been updated to use the new a: " + c);
 function sayHey() {
     console.log("Hey!");
 }
+
+
+sayHey()
